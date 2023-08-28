1163fcd6e6a4d531b325b34d82bb7d533fe9dd21
KaisAbiyyi <kaisabiyyi2@gmail.com>
2023-08-28 17:02:03 +0700

    diagram changes

--- src/components/diagram/DiagramCard.tsx
+++ src/components/diagram/DiagramCard.tsx
@@ -21,18 +21,23 @@ interface DiagramProps {
 const DiagramCard = (data: Array<DiagramCardProps>, id: string | number | undefined) => {
     const LastChildrenIds = data.filter(parentItem => {
         const children = data.filter(childItem => childItem.parent === parentItem.id);
-
         const hasChildren = children.length > 0;
         const hasNestedChildren = children.some(child => data.some(item => item.parent === child.id));
 
         return hasChildren && !hasNestedChildren;
     }).map(item => item.id);
 
+    const debug = data
+        .filter(item => item.parent === id)
+        .map((item: DiagramCardProps) => {
+            console.log(data.filter(filter => filter.parent === item.id).length > 1)
+        })
+
     return data
         .filter(item => item.parent === id)
         .map((item: DiagramCardProps) => (
-            <div className="flex flex-col items-center flex-grow gap-8" key={item.id}>
-                <div className="flex gap-8 p-3 rounded-lg shadow w-72 bg-slate-50">
+            <div className={`flex flex-col items-center justify-start flex-grow  `} key={item.id}>
+                <div className="relative flex p-3 rounded-lg shadow w-72 bg-slate-50">
                     <div className="relative w-24 overflow-hidden rounded-lg bg-slate-200">
                         {item?.photo &&
                             <Image src={item?.photo} alt={item?.name} fill style={{ objectFit: 'cover' }} />
@@ -44,9 +49,14 @@ const DiagramCard = (data: Array<DiagramCardProps>, id: string | number | undefi
                         <p className="m-0 text-xs font-medium text-slate-500">{item?.phone}</p>
                         <p className="m-0 text-xs font-medium text-slate-500">{item?.mail}</p>
                     </div>
+                    {/* <div className="absolute top-0 w-1 h-8 -translate-y-full left-1/2 bg-slate-500"></div>
+                    <div className="absolute bottom-0 w-1 h-8 translate-y-full left-1/2 bg-slate-500"></div> */}
                 </div>
-                <div className={`flex gap-8 relative w-full ${LastChildrenIds.includes(item.id) ? 'flex-col pl-8' : ''}`}>
-                    {LastChildrenIds.includes(item.parent)}
+                <p>
+                </p>
+                {data.filter(filter => filter.parent === item.id).length > 1 && !LastChildrenIds.includes(item.id) ? <div className="w-4/6 h-6 m-auto bg-red-500"></div> : ''}
+
+                <div className={`flex relative w-full ${LastChildrenIds.includes(item.id) ? 'flex-col items-center pl-8' : ''}`}>
                     {DiagramCard(data, item.id)}
                 </div>
             </div>
@@ -59,22 +69,26 @@ export default function DiagramOrganization(props: DiagramProps) {
 
     return (
         <>
-            <div className={"relative flex flex-col items-center flex-grow gap-8 overflow-x-scroll " + scrollbarClass}>
-                <div className="flex gap-8 p-3 rounded-lg shadow bg-slate-50">
-                    <div className="relative w-24 overflow-hidden rounded-lg bg-slate-200">
-                        {MainCard?.photo &&
-                            <Image src={MainCard?.photo} alt={MainCard?.name} fill style={{ objectFit: 'cover' }} />
-                        }
+            <div className="p-4 m-auto max-w-5/6">
+                <div className={"relative flex flex-col w-auto whitespace-nowrap flex-nowrap overflow-x-scroll " + scrollbarClass}>
+                    <div className="relative flex p-3 m-auto rounded-lg shadow w-72 bg-slate-50">
+                        <div className="relative w-24 overflow-hidden rounded-lg bg-slate-200">
+                            {MainCard?.photo &&
+                                <Image src={MainCard?.photo} alt={MainCard?.name} fill style={{ objectFit: 'cover' }} />
+                            }
+                        </div>
+                        <div className="flex flex-col gap-1">
+                            <p className="m-0 text-xs font-semibold text-slate-600">{MainCard?.name}</p>
+                            <p className="m-0 text-xs font-semibold text-slate-500">{MainCard?.title}</p>
+                            <p className="m-0 text-xs font-medium text-slate-500">{MainCard?.phone}</p>
+                            <p className="m-0 text-xs font-medium text-slate-500">{MainCard?.mail}</p>
+                        </div>
+                        <div className="absolute bottom-0 w-1 h-8 translate-y-full left-1/2 bg-slate-500"></div>
                     </div>
-                    <div className="flex flex-col gap-1">
-                        <p className="m-0 text-xs font-semibold text-slate-600">{MainCard?.name}</p>
-                        <p className="m-0 text-xs font-semibold text-slate-500">{MainCard?.title}</p>
-                        <p className="m-0 text-xs font-medium text-slate-500">{MainCard?.phone}</p>
-                        <p className="m-0 text-xs font-medium text-slate-500">{MainCard?.mail}</p>
+                    {/* <div className="w-4/6 h-10 m-auto bg-red-500"></div> */}
+                    <div className="relative flex w-full ">
+                        {DiagramCard(data, 'main')}
                     </div>
-                </div>
-                <div className="relative flex w-full gap-8">
-                    {DiagramCard(data, 'main')}
                 </div>
             </div>
         </>
