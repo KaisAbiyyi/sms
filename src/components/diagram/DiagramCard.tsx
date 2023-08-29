443b00c964caf0275d1c2e0e54dcd9bb877902be
KaisAbiyyi <kaisabiyyi2@gmail.com>
2023-08-29 09:15:47 +0700

    diagram changes

--- src/components/diagram/DiagramCard.tsx
+++ src/components/diagram/DiagramCard.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image"
 import { scrollbarClass } from "../ElementClass"
+import { saveAs } from 'file-saver'
 
 export type DiagramCardProps = {
     id: string | number | undefined
@@ -30,14 +31,20 @@ const DiagramCard = (data: Array<DiagramCardProps>, id: string | number | undefi
     const debug = data
         .filter(item => item.parent === id)
         .map((item: DiagramCardProps) => {
-            console.log(data.filter(filter => filter.parent === item.id).length > 1)
+            if (!LastChildrenIds.includes(item.parent)) {
+                return item
+            }
         })
+    const sliced = debug.slice(1, -1).filter(filter => filter !== undefined).map(item => item?.id)
 
     return data
         .filter(item => item.parent === id)
         .map((item: DiagramCardProps) => (
-            <div className={`flex flex-col items-center justify-start flex-grow  `} key={item.id}>
-                <div className="relative flex p-3 rounded-lg shadow w-72 bg-slate-50">
+            <div className={`flex flex-col items-center justify-start h-fit `} key={item.id}>
+                <div className="relative flex h-32 gap-3 p-3 rounded-lg shadow w-72 bg-slate-50">
+                    {sliced.includes(item.id) &&
+                        <div className="absolute top-0 z-10 w-1 h-8 -translate-y-full left-1/2 bg-slate-300"></div>
+                    }
                     <div className="relative w-24 overflow-hidden rounded-lg bg-slate-200">
                         {item?.photo &&
                             <Image src={item?.photo} alt={item?.name} fill style={{ objectFit: 'cover' }} />
@@ -49,12 +56,19 @@ const DiagramCard = (data: Array<DiagramCardProps>, id: string | number | undefi
                         <p className="m-0 text-xs font-medium text-slate-500">{item?.phone}</p>
                         <p className="m-0 text-xs font-medium text-slate-500">{item?.mail}</p>
                     </div>
-                    {/* <div className="absolute top-0 w-1 h-8 -translate-y-full left-1/2 bg-slate-500"></div>
-                    <div className="absolute bottom-0 w-1 h-8 translate-y-full left-1/2 bg-slate-500"></div> */}
+                    {/* <div className="absolute top-0 w-1 h-8 -translate-y-full left-1/2 bg-slate-300"></div>
+                    <div className="absolute bottom-0 w-1 h-8 translate-y-full left-1/2 bg-slate-300"></div> */}
                 </div>
-                <p>
-                </p>
-                {data.filter(filter => filter.parent === item.id).length > 1 && !LastChildrenIds.includes(item.id) ? <div className="w-4/6 h-6 m-auto bg-red-500"></div> : ''}
+                {data.filter(filter => filter.parent === item.id).length > 1 && !LastChildrenIds.includes(item.id) ?
+                    <>
+                        <div className="w-1 h-8 m-auto bg-slate-300"></div>
+                        <div className="w-4/6 h-1 m-auto bg-slate-300"></div>
+                        <div className="flex justify-between w-4/6 m-auto">
+                            <div className="w-1 h-8 bg-slate-300"></div>
+                            <div className="w-1 h-8 bg-slate-300"></div>
+                        </div>
+                    </>
+                    : ''}
 
                 <div className={`flex relative w-full ${LastChildrenIds.includes(item.id) ? 'flex-col items-center pl-8' : ''}`}>
                     {DiagramCard(data, item.id)}
@@ -71,7 +85,7 @@ export default function DiagramOrganization(props: DiagramProps) {
         <>
             <div className="p-4 m-auto max-w-5/6">
                 <div className={"relative flex flex-col w-auto whitespace-nowrap flex-nowrap overflow-x-scroll " + scrollbarClass}>
-                    <div className="relative flex p-3 m-auto rounded-lg shadow w-72 bg-slate-50">
+                    <div className="relative flex gap-3 p-3 m-auto rounded-lg shadow w-72 bg-slate-50">
                         <div className="relative w-24 overflow-hidden rounded-lg bg-slate-200">
                             {MainCard?.photo &&
                                 <Image src={MainCard?.photo} alt={MainCard?.name} fill style={{ objectFit: 'cover' }} />
@@ -83,10 +97,14 @@ export default function DiagramOrganization(props: DiagramProps) {
                             <p className="m-0 text-xs font-medium text-slate-500">{MainCard?.phone}</p>
                             <p className="m-0 text-xs font-medium text-slate-500">{MainCard?.mail}</p>
                         </div>
-                        <div className="absolute bottom-0 w-1 h-8 translate-y-full left-1/2 bg-slate-500"></div>
                     </div>
-                    {/* <div className="w-4/6 h-10 m-auto bg-red-500"></div> */}
-                    <div className="relative flex w-full ">
+                    <div className="w-1 h-8 m-auto bg-slate-300"></div>
+                    <div className="w-4/6 h-1 m-auto bg-slate-300"></div>
+                    <div className="flex justify-between w-4/6 m-auto">
+                        <div className="w-1 h-8 bg-slate-300"></div>
+                        <div className="w-1 h-8 bg-slate-300"></div>
+                    </div>
+                    <div className="relative flex w-full">
                         {DiagramCard(data, 'main')}
                     </div>
                 </div>
